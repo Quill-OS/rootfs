447bdaca048940af6f7fc971976df4377cbca604
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-07-13 13:04:38 -0400

    Rotation things again

--- etc/init.d/splash.sh
+++ etc/init.d/splash.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-UPDATE_SPLASH=`cat /opt/update/will_update` 2>/dev/null
-echo 0 > /sys/class/graphics/fb0/rotate
+UPDATE_SPLASH=$(cat /opt/update/will_update 2>/dev/null)
+DEVICE=$(cat /opt/inkbox_device)
+if [ "${DEVICE}" == "n705" ] || [ "${DEVICE}" == "n905b" ] || [ "${DEVICE}" == "n905c" ] || [ "${DEVICE}" == "n613" ]; then
+	FB_UR=3
+elif [ "${DEVICE}" == "n873" ]; then
+	FB_UR=0
+else
+	FB_UR=0
+fi
 
-if [ "$UPDATE_SPLASH" == "true" ]; then
+echo ${FB_UR} > /sys/class/graphics/fb0/rotate
+
+if [ "${UPDATE_SPLASH}" == "true" ]; then
 	killall -q update-splash
 	/opt/bin/fbink/fbink -k -f -h -q
 	/opt/bin/fbink/fbink -t regular=/etc/init.d/splash.d/fonts/resources/inter-b.ttf,size=20 "Updating" -m -M -h -q
