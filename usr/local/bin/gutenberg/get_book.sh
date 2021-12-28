7d10417f289b468417d60b72275b08a17186044d
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-12-28 18:13:49 -0500

    get_book.sh: Fetch cover instead if specified

--- usr/local/bin/gutenberg/get_book.sh
+++ usr/local/bin/gutenberg/get_book.sh
@@ -3,4 +3,9 @@
 [ -z "${1}" ] && echo "Error. Please provide the 'id' argument." && exit 1
 ID="${1}"
 
-wget -O "/data/onboard/${ID}.epub" "http://gutenberg.org/ebooks/${ID}.epub.noimages"
+if [ "${2}" != "cover" ]; then
+	wget -O "/data/onboard/${ID}.epub" "http://gutenberg.org/ebooks/${ID}.epub.noimages"
+else
+	mkdir -p /kobo/inkbox/gutenberg
+	wget -O "/kobo/inkbox/gutenberg/book_cover.jpg" "http://gutenberg.org/files/${ID}/${ID}-h/images/cover.jpg"
+fi
