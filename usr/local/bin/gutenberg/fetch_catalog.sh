964609de098e46c0549972cef99a5427cf4eb78e
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-12-30 21:12:11 -0500

    ibxd: Write appropriate boolean values to gutenbergSyncDone

--- usr/local/bin/gutenberg/fetch_catalog.sh
+++ usr/local/bin/gutenberg/fetch_catalog.sh
@@ -7,7 +7,7 @@ date +%s > /data/storage/gutenberg/last_sync
 
 # Fetching catalog
 rm -f catalog.csv
-wget -O catalog.csv http://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv
+wget -O catalog.csv http://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv || exit 1
 
 cd -
 sync
