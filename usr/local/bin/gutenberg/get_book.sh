7d2493432841eafc6a1239dfc2e4cd9aa2f2c1e7
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-01-30 10:31:12 -0500

    Gutenberg: Remove book if download failed

--- usr/local/bin/gutenberg/get_book.sh
+++ usr/local/bin/gutenberg/get_book.sh
@@ -5,6 +5,7 @@ ID="${1}"
 
 if [ "${2}" != "cover" ]; then
 	wget -O "/data/onboard/${ID}.epub" "http://gutenberg.org/ebooks/${ID}.epub.noimages"
+	[ ${?} != 0 ] && rm -f "/data/onboard/${ID}.epub" && exit 1
 else
 	mkdir -p /kobo/inkbox/gutenberg
 	wget -O "/kobo/inkbox/gutenberg/book_cover.jpg" "http://gutenberg.org/files/${ID}/${ID}-h/images/cover.jpg"
