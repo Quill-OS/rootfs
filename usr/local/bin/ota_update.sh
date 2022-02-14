17292666e5fd3bf30f6dfc5f2fe6677d9730f3e3
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-02-13 21:49:53 -0500

    ota_update.sh: Some error handling

--- usr/local/bin/ota_update.sh
+++ usr/local/bin/ota_update.sh
@@ -11,9 +11,15 @@ if [ "${UPDATE_COMP}" == "1" ]; then
 	echo "true" > /kobo/run/can_ota_update
 	if [ "${1}" == "download" ]; then
 		busybox wget "http://${SERVER}/bundles/inkbox/native/update/${OTA_CURRENT}/${FQDI}/inkbox-update-${OTA_CURRENT}.upd.isa" -O "/data/onboard/.inkbox/inkbox-update-${OTA_CURRENT}.upd.isa"
-		echo "true" > /data/onboard/.inkbox/can_update
-		rc-service update_inkbox restart
-		echo "true" > /kobo/run/can_install_ota_update
+		if [ ${?} != 0 ]; then
+			rm -f "/data/onboard/.inkbox/inkbox-update-${OTA_CURRENT}.upd.isa"
+			echo "false" > /kobo/run/can_install_ota_update
+			exit 1
+		else
+			echo "true" > /data/onboard/.inkbox/can_update
+			rc-service update_inkbox restart
+			echo "true" > /kobo/run/can_install_ota_update
+		fi
 	fi
 else
 	echo "false" > /data/onboard/.inkbox/can_update
