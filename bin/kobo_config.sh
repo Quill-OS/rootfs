aef8f07c10e4f81f811bcde5a66d7538d756e843
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-02-15 21:57:31 -0500

    X11 & GUI: Make it work depending on the device

---
+++ bin/kobo_config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DEVICE=$(cat /opt/inkbox_device)
+
+if [ "${DEVICE}" = "n705" ]; then
+	echo "pixie"
+elif [ "${DEVICE}" = "n905b" ] || [ "${DEVICE}" = "n905c" ] || [ "${DEVICE}" = "n905" ]; then
+	echo "trilogy"
+elif [ "${DEVICE}" = "n613" ]; then
+	echo "daylight"
+elif [ "${DEVICE}" = "n236" ]; then
+	echo "star"
+elif [ "${DEVICE}" = "n437" ]; then
+	echo "alyssum"
+elif [ "${DEVICE}" = "n873" ]; then
+	echo "storm"
+fi
