f5c9ecec383da4ee799defc8e1f2e99d1c83c454
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-02-27 21:48:42 -0500

    Some fixes for shell messages

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -14,3 +14,6 @@
 # end
 
 set fish_greeting
+if ! [ (ifsctl mnt rootfs stat) = "Root filesystem is mounted read-write." ]
+        echo -e "\033[1m* Warning *\033[0m\nRoot filesystem is mounted read-only.\nInvoke `ifsctl mnt rootfs rw' to make it read-write."
+end
--- etc/profile.d/rootfs_check.sh
+++ etc/profile.d/rootfs_check.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ROOTFS_STATUS=$(/usr/bin/ifsctl mnt rootfs stat)
-READ_WRITE_SUB="read-write"
 
 if [ "$ROOTFS_STATUS" != "Root filesystem is mounted read-write." ]; then
         echo -e "\033[1m* Warning *\033[0m\nRoot filesystem is mounted read-only.\nInvoke \`ifsctl mnt rootfs rw' to make it read-write."
