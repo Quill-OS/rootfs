ac8abf0c7aaf93ed8bcbd7c2a8bc1dda29925086
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-02-27 20:56:21 -0500

    fish_prompt.fish: Improve prompt

--- usr/share/fish/functions/fish_prompt.fish
+++ usr/share/fish/functions/fish_prompt.fish
@@ -5,12 +5,14 @@ function fish_prompt
         set_color normal
         echo -n "@"
         set_color F67400
-        echo (hostname) (set_color green; pwd)(set_color normal)'>' (set_color normal)
+	set current_path (set current_path_raw (pwd) && if [ "$current_path_raw" = "$HOME" ]; echo -n "~"; else; echo -n "$current_path_raw"; end)
+        echo (hostname) (set_color green; echo -n "$current_path")(set_color normal)'>' (set_color normal)
     else
         set_color brgreen
         echo -n "$USER"
         set_color normal
         echo -n "@"
-        echo (hostname) (set_color red; pwd)(set_color normal)'#' (set_color normal)
+	set current_path (set current_path_raw (pwd) && if [ "$current_path_raw" = "$HOME" ]; echo -n "~"; else; echo -n "$current_path_raw"; end)
+        echo (hostname) (set_color red; echo -n "$current_path")(set_color normal)'#' (set_color normal)
     end
 end
