926a17b620052557057494868db28a1651b477b1
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-02-28 23:17:48 -0500

    Fixes related to tmpfs size and fish

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,19 +1,6 @@
-# Put system-wide fish configuration entries here
-# or in .fish files in conf.d/
-# Files in conf.d can be overridden by the user
-# by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
-
-# This file is run by all fish instances.
-# To include configuration only for login shells, use
-# if status --is-login
-#    ...
-# end
-# To include configuration only for interactive shells, use
-# if status --is-interactive
-#   ...
-# end
-
 set fish_greeting
-if ! [ (ifsctl mnt rootfs stat) = "Root filesystem is mounted read-write." ]
-        echo -e "\033[1m* Warning *\033[0m\nRoot filesystem is mounted read-only.\nInvoke `ifsctl mnt rootfs rw' to make it read-write."
+if status --is-interactive
+	if ! [ (ifsctl mnt rootfs stat) = "Root filesystem is mounted read-write." ]
+	        echo -e "\033[1m* Warning *\033[0m\nRoot filesystem is mounted read-only.\nInvoke `ifsctl mnt rootfs rw' to make it read-write."
+	end
 end
