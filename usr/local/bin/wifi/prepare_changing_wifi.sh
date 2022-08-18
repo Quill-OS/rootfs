2d62e1b87e729b69ad30fe70fa79e5d93fc6f3a4
Szybet <53944559+Szybet@users.noreply.github.com>
2022-08-18 21:46:08 +0200

    further fixes and work

--- usr/local/bin/wifi/prepare_changing_wifi.sh
+++ usr/local/bin/wifi/prepare_changing_wifi.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+killall -9 connect_to_network.sh connection_manager.sh only_connect_to_network.sh smarter_time_sync.sh
 killall -q dhcpcd wpa_supplicant udhcpc
 killall -q dhcpcd wpa_supplicant udhcpc
 sleep 0.5
@@ -7,3 +8,6 @@ killall -9 dhcpcd wpa_supplicant udhcpc
 
 rm /run/wifi_stats
 touch /run/wifi_stats
+
+rm -f /run/wifi_logs
+touch /run/wifi_logs
