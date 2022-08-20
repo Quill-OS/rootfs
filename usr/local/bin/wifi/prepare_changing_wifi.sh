b4523b6bd13222ec8befde561fe52098c82520b2
Szybet <53944559+Szybet@users.noreply.github.com>
2022-08-20 21:20:01 +0200

    mdjksamk checking password while connecting

--- usr/local/bin/wifi/prepare_changing_wifi.sh
+++ usr/local/bin/wifi/prepare_changing_wifi.sh
@@ -10,23 +10,17 @@ fi
 # I cant explain why this script cant kill connection_manager, so this file is needed. i dont want to know. please leave it :(
 echo "true" > /run/stopping_wifi
 
-killall -9 connect_to_network.sh connection_manager.sh
-killall -9 connect_to_network.sh connection_manager.sh
+killall -9 connect_to_network.sh connection_manager.sh check_wifi_password.sh prepare_network.sh
+killall -9 connect_to_network.sh connection_manager.sh check_wifi_password.sh prepare_network.sh
 sleep 0.3
 killall -9 connection_manager.sh
 killall -9 connection_manager.sh
 # This needs to be separate, because it will trigger toggle off otherwise
-killall -9  get_dhcp.sh smarter_time_sync.sh
-killall -9  get_dhcp.sh smarter_time_sync.sh
+killall -9  get_dhcp.sh smarter_time_sync.sh check_wifi_password.sh prepare_network.sh
+killall -9  get_dhcp.sh smarter_time_sync.sh check_wifi_password.sh prepare_network.sh
 
 wpa_cli disconnect; wpa_cli logoff; wpa_cli disable_network 0; wpa_cli remove_network 0; wpa_cli terminate; ip addr flush dev "${WIFI_DEV}"
 killall -q dhcpcd wpa_supplicant udhcpc iwevent
 killall -q dhcpcd wpa_supplicant udhcpc iwevent
 sleep 0.5
 killall -9 dhcpcd wpa_supplicant udhcpc iwevent
-
-rm /run/wifi_stats
-touch /run/wifi_stats
-
-rm -f /run/wifi_logs
-touch /run/wifi_logs
