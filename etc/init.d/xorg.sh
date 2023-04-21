de62eda65ecb554eee30237aead8ce7d17a72103
Nicolas Mailloux <nicolecrivain@gmail.com>
2023-04-21 23:49:26 +0000

    X11 fixes final touch

--- etc/init.d/xorg.sh
+++ etc/init.d/xorg.sh
@@ -30,11 +30,11 @@ fi
 echo $FB_UR > /sys/class/graphics/fb0/rotate
 
 if [ "$DPI" == "" ]; then
-        chroot /xorg "X" "-v" "-nocursor" &
+        chroot /xorg "X" "-nocursor" &
 	DISPLAY=:0 chroot /xorg "x11vnc" "-localhost" "-forever" "-quiet" &
 	sleep 20
 else
-        chroot /xorg "X" "-v" "-nocursor" "-dpi" "$DPI" &
+        chroot /xorg "X" "-nocursor" "-dpi" "$DPI" &
 	DISPLAY=:0 chroot /xorg "x11vnc" "-localhost" "-forever" "-quiet" &
 	sleep 20
 fi
