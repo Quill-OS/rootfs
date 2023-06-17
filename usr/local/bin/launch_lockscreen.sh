937c527bffd50d342c2da6c56b61977a906a9068
Nicolas Mailloux <nicolecrivain@gmail.com>
2023-06-17 10:35:05 -0400

    `launch_lockscreen.sh`: Dark mode fix

--- usr/local/bin/launch_lockscreen.sh
+++ usr/local/bin/launch_lockscreen.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 DEVICE="$(cat /opt/inkbox_device)"
-DARK_MODE="$(cat /data/config/10-dark_mode/config)"
+
+if [ -e "/var/run/openrc/started/inkbox_gui" ]; then
+	DARK_MODE="$(cat /kobo/mnt/onboard/.adds/inkbox/.config/10-dark_mode/config)"
+else
+	DARK_MODE="$(cat /data/config/10-dark_mode/config)"
+fi
 
 if [ "${DEVICE}" != "emu" ]; then
 	env QT_QPA_PLATFORM="kobo:touchscreen_rotate=90:touchscreen_invert_x=auto:touchscreen_invert_y=auto:logicaldpitarget=0" QT_FONT_DPI=${DPI} ADDSPATH="/mnt/onboard/.adds/" QTPATH="${ADDSPATH}/qt-linux-5.15.2-kobo/" LD_LIBRARY_PATH="${QTPATH}lib:/lib" chroot /kobo /mnt/onboard/.adds/inkbox/lockscreen
