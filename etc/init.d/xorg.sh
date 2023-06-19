e773ee084977931b1ac264957d19ba6a7f8ca487
Nicolas Mailloux <nicolecrivain@gmail.com>
2023-06-19 18:27:09 -0400

    `xorg.sh` fix

--- etc/init.d/xorg.sh
+++ etc/init.d/xorg.sh
@@ -106,9 +106,14 @@ fi
 
 echo $FB_UR > /sys/class/graphics/fb0/rotate
 if [ "${DEVICE}" == "n437" ] || [ "${DEVICE}" == "n306" ]; then
-	# Don't even try to understand this
-	if grep -q "true" /opt/root/rooted && ! grep -q "true" /tmp/kobox_initial_launch_done; then
+	## Don't even try to understand this
+	# This runs when the device IS rooted * AND * it's the FIRST launch of an X11 app since boot * AND * we are NOT running on a Kobo Nia
+	if grep -q "true" /opt/root/rooted && ! grep -q "true" /tmp/kobox_initial_launch_done && ! grep -q "n306" /opt/inkbox_device; then
 		/opt/bin/fbink/fbdepth -d 16
+	# This runs when either the device is NOT rooted * OR * it's the FIRST launch of an X11 app since boot * OR * we are running on a Kobo Nia
+	else if ! grep -q "true" /tmp/kobox_initial_launch_done; then
+		/opt/bin/fbink/fbdepth -d 16
+	# This runs when the device (rooted or not) ALREADY launched an X11 app since boot
 	else
 		/opt/bin/fbink/fbdepth -d 32
 	fi
