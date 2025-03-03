e0808917c656568f45b52b80fd7119229fb76643
Nicolas Mailloux <nicolecrivain@gmail.com>
2025-03-02 22:35:08 -0500

    KOReader: More fixes

--- usr/local/share/koreader-overlay/settings.reader.lua
+++ usr/local/share/koreader-overlay/settings.reader.lua
@@ -1,5 +1,9 @@
 -- ./settings.reader.lua
 return {
+    ["footer"] = {
+        ["progress_style_thin"] = true,
+        ["progress_style_thin_height"] = 4,
+    },
     ["plugins_disabled"] = {
         ["SSH"] = true,
         ["archiveviewer"] = true,
@@ -25,5 +29,13 @@ return {
         ["texteditor"] = true,
         ["vocabbuilder"] = true,
         ["wallabag"] = true,
+        ["calibrecompanion"] = true,
+        ["evernote"] = true,
+        ["exporter"] = true,
+        ["goodreads"] = true,
+        ["kobolight"] = true,
+        ["send2ebook"] = true,
+        ["storagestat"] = true,
+        ["zsync"] = true,
     },
 }
