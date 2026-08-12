b61a87bbe3636a5b243aba197f4226ca96bf8543
Nicolas Mailloux <nicolecrivain@gmail.com>
2026-08-12 11:48:19 -0400

    Add sign capability

--- release.sh
+++ release.sh
@@ -6,6 +6,7 @@ chmod u+s "bin/busybox"
 find . -type f -name ".keep" -exec rm {} \;
 rm -f ../rootfs.squashfs
 mksquashfs . ../rootfs.squashfs -b 1048576 -comp xz -Xdict-size 100% -always-use-fragments -all-root -e .git -e .gitignore -e .vscode -e release.sh
+[ -e "${1}" ] && openssl dgst -sha256 -sign "${1}" -out ../rootfs.squashfs.dgst ../rootfs.squashfs
 rm ./.commit
 find . -type d ! -path "*.git*" -empty -exec touch '{}'/.keep \;
 echo "Root filesystem has been compressed."
