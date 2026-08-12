949ab3c470639c0b354a56f972b18030698625a6
Nicolas Mailloux <nicolecrivain@gmail.com>
2026-08-12 11:46:37 -0400

    Improve script

--- release.sh
+++ release.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if [ -z "${GITDIR}" ]; then
-	echo "Please specify the 'GITDIR' environment variable."
-	exit 1
-fi
-
-cd "${GITDIR}"
+cd "$(dirname ""${0}"")"
 git rev-parse HEAD > ./.commit
-chmod u+s "${GITDIR}/bin/busybox"
+chmod u+s "bin/busybox"
 find . -type f -name ".keep" -exec rm {} \;
 rm -f ../rootfs.squashfs
 mksquashfs . ../rootfs.squashfs -b 1048576 -comp xz -Xdict-size 100% -always-use-fragments -all-root -e .git -e .gitignore -e .vscode -e release.sh
