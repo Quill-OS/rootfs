89a6a3f47d89d85a6139bbbc5f622741b563d399
Nicolas Mailloux <nicolecrivain@gmail.com>
2026-08-14 13:22:30 -0400

    N367, N428: More compatibility things

--- bin/kobo_config.sh
+++ bin/kobo_config.sh
@@ -6,22 +6,26 @@ else
 	DEVICE=$(cat /opt/inkbox_device)
 fi
 
-if [ "${DEVICE}" = "n705" ]; then
+if [ "${DEVICE}" == "n705" ]; then
 	echo "pixie"
-elif [ "${DEVICE}" = "n905b" ] || [ "${DEVICE}" = "n905c" ] || [ "${DEVICE}" = "n905" ]; then
+elif [ "${DEVICE}" == "n905b" ] || [ "${DEVICE}" == "n905c" ] || [ "${DEVICE}" == "n905" ]; then
 	echo "trilogy"
-elif [ "${DEVICE}" = "n613" ]; then
+elif [ "${DEVICE}" == "n613" ]; then
 	echo "daylight"
-elif [ "${DEVICE}" = "n236" ]; then
+elif [ "${DEVICE}" == "n236" ]; then
 	echo "star"
-elif [ "${DEVICE}" = "n437" ]; then
+elif [ "${DEVICE}" == "n437" ]; then
 	echo "alyssum"
-elif [ "${DEVICE}" = "n306" ]; then
+elif [ "${DEVICE}" == "n306" ]; then
 	echo "luna"
 elif [ "${DEVICE}" == "n249" ]; then
 	echo "nova"
-elif [ "${DEVICE}" = "n873" ]; then
+elif [ "${DEVICE}" == "n873" ]; then
 	echo "storm"
 elif [ "${DEVICE}" == "n418" ]; then
 	echo "io"
+elif [ "${DEVICE}" == "n367" ]; then
+	echo "spa"
+elif [ "${DEVICE}" == "n428" ]; then
+	echo "monza"
 fi
